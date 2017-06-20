87f86c3abff3900fd341818d07141bd7cfab00f3
sjain <sapan.jain@charter.com>
2017-06-20 11:41:38 -0500

    adding initial project structure

---
+++ src/main/java/com/ferguson/evaluation/flightsystem/context/FlightSystemApplication.java
@@ -0,0 +1,12 @@
+package com.ferguson.evaluation.flightsystem.context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSystemApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ferguson/evaluation/flightsystem/service/FlightSystemService.java
@@ -0,0 +1,7 @@
+package com.ferguson.evaluation.flightsystem.service;
+
+/**
+ * Created by sjain on 6/20/2017.
+ */
+public class FlightSystemService {
+}
